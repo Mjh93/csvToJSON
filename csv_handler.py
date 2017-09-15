cc17b3a4614313368b3b39ecf704bda6de9a4dd4
Michael Holland <Mick@Michaels-MacBook-Pro.local>
2017-09-15 10:28:55 +0100

    Committing items I forgot to add

--- csv_handler.py
+++ csv_handler.py
@@ -6,5 +6,5 @@ with open('us-500.csv', 'rU') as csv_file:
         for row in csv_reader:
             json_str = json.dumps(row)
             row = json.loads(json_str)
-            print ', '.join(row)
+            # print ', '.join(row)
 
