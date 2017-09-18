77cf9ff8420623f6f5a4afd51529cc0809b5412b
Michael Holland <Mick@Michaels-MacBook-Pro.local>
2017-09-18 14:36:20 +0100

    Simple server responding to POSTS

--- post.py
+++ post.py
@@ -9,32 +9,34 @@ def post_to_server(json_object, url):
     conn = httplib.HTTPConnection(url)
     conn.request("POST", "", params, headers)
     response = conn.getresponse()
+    if response.status != 200:
+        sys.exit(-2)
     print response.status, response.reason
     data = response.read()
     conn.close()
 
 
 def main():
-    json_object = ""
-    with open(sys.argv[1], 'rt') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',', quotechar='\n')
-        json_object = json.dumps(next(csv_reader))
-        for entry in csv_reader:
-            processed_data = process_entry(entry)
-            json_object.add(processed_data)
-
-    return json_object
+    csv_file = open(sys.argv[1], 'rU')
+    json_file = open('file.json', 'w')
+    reader = csv.reader(csv_file)
+    fieldnames = next(reader)
+    reader = csv.DictReader(csv_file, fieldnames)
+    for row in reader:
+        json.dump(process_entry(row), json_file)
+        json_file.write('\n')  #TODO this should be changed if handling large csv files
+    return json_file
 
 
 def process_entry(entry):
-    print("Some generic unknown processing here")
+    # print("Some generic unknown processing here")
     return(entry)
 
 
 if __name__ == "__main__":
     if sys.argv.__len__() > 1:
         json_object = main()
-        post_to_server(json_object, "127.0.0.1:7800")
+        post_to_server(json_object, sys.argv[2])
         sys.exit(0)
     else:
         print("FAILED: No csv file given.")
